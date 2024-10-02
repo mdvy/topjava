83007922b9c46f9801e843aa484ad8824b01ac86
mdvy <9508923633@ya.ru>
2024-10-02 16:33:55 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
@@ -27,4 +27,8 @@ public class UserMealWithExcess {
                 ", excess=" + excess +
                 '}';
     }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
 }
