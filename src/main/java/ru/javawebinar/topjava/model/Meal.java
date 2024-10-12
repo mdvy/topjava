609b1f608bc6e51c255392ed49a22dfde9760c1c
mdvy <9508923633@ya.ru>
2024-10-12 13:20:48 +0300

    extend Meal from AbstractBaseEntity

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,9 +5,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Objects;
 
-public class Meal {
-    private Integer id;
-
+public class Meal extends AbstractBaseEntity{
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -21,19 +19,11 @@ public class Meal {
     }
 
     public Meal(Integer id, Integer userId, LocalDateTime dateTime, String description, int calories) {
-        this.id = id;
+        super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.userId = userId;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
@@ -54,10 +44,6 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
-    }
-
-    public boolean isNew() {
-        return id == null;
     }
 
     public Integer getUserId() {
