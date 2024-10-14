ff15217b882e03db2a2696912f3423769306b7f0
mdvy <9508923633@ya.ru>
2024-10-14 23:42:35 +0300

    clean SpringMain

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,19 +1,15 @@
 package ru.javawebinar.topjava;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
 public class SpringMain {
-    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
