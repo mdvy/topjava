4d96c15837bc341fff069818ac8c66f6a01f9575
mdvy <9508923633@ya.ru>
2024-10-27 14:32:17 +0300

    add benchmark for MealServiceTest

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,13 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,6 +19,9 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
@@ -27,9 +36,33 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 
 public class MealServiceTest {
+    private static final Logger log = LoggerFactory.getLogger("benchmark");
+    private static final List<String> benchmark_times = new ArrayList<>();
 
     @Autowired
     private MealService service;
+
+    private static void logInfo(Description description, long nanos) {
+        String testName = description.getMethodName();
+        String info = String.format("Test %s, spent %d ms",
+                testName, TimeUnit.NANOSECONDS.toMillis(nanos));
+        log.info(info);
+        benchmark_times.add(info);
+    }
+
+    @Rule
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            logInfo(description, nanos);
+        }
+    };
+
+    @AfterClass
+    public static void benchmarkResults() {
+        log.info("\n##### BENCHMARK RESULTS #####\n");
+        benchmark_times.forEach(log::info);
+    }
 
     @Test
     public void delete() {
