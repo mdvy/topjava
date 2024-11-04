513b7d15229dfd03dda1e2c27f3c4d2ab14c9f1b
mdvy <9508923633@ya.ru>
2024-11-05 00:40:03 +0300

    add datajpa tests

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -5,7 +5,10 @@ import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.MealServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import static org.junit.Assert.assertThrows;
+import static ru.javawebinar.topjava.MealTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
 import static ru.javawebinar.topjava.UserTestData.*;
@@ -13,10 +16,20 @@ import static ru.javawebinar.topjava.UserTestData.*;
 @ActiveProfiles(value = DATAJPA)
 public class DataJpaMealServiceTest extends MealServiceTest {
     @Test
-    public void getByIdAndUserIdWithUserTest() {
+    public void getByIdAndUserIdWithUser() {
         Meal actualMeal = service.getByIdAndUserIdWithUser(MEAL1_ID, USER_ID);
         MEAL_MATCHER.assertMatch(actualMeal, meal1);
         User actualUser = actualMeal.getUser();
         USER_MATCHER.assertMatch(actualUser, user);
+    }
+
+    @Test
+    public void getByIdAndUserIdWithUserNotFound() {
+        assertThrows(NotFoundException.class, () -> service.getByIdAndUserIdWithUser(NOT_FOUND, USER_ID));
+    }
+
+    @Test
+    public void getByIdAndUserIdWithUserNotOwn() {
+        assertThrows(NotFoundException.class, () -> service.getByIdAndUserIdWithUser(MEAL1_ID, ADMIN_ID));
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -7,21 +7,36 @@ import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
-import static ru.javawebinar.topjava.UserTestData.USER_ID;
-import static ru.javawebinar.topjava.UserTestData.USER_MATCHER;
+import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(value = DATAJPA)
 public class DataJpaUserServiceTest extends UserServiceTest {
     @Test
-    public void getByIdWithMealsTest() {
+    public void getByIdWithMeals() {
         User actualUser = service.getByIdWithMeals(USER_ID);
         USER_MATCHER.assertMatch(actualUser, UserTestData.user);
         List<Meal> actualMeals = actualUser.getMeals();
         MEAL_MATCHER.assertMatch(actualMeals, MealTestData.meals);
+    }
+
+    @Test
+    public void getByIdWithMealsNotFound() {
+        assertThrows(NotFoundException.class, () -> service.getByIdWithMeals(NOT_FOUND));
+    }
+
+    @Test
+    public void getByIdWithMealsEmptyMeals() {
+        User actualUser = service.getByIdWithMeals(GUEST_ID);
+        USER_MATCHER.assertMatch(actualUser, guest);
+        List<Meal> actualMeals = actualUser.getMeals();
+        MEAL_MATCHER.assertMatch(actualMeals, Collections.emptyList());
     }
 }
